cb4c8256100488381b3d3c23c8693441eca1724e
Dillon Huff <dillonhuff@gmail.com>
2013-09-03 15:15:20 -0700

    VerySimpleRetrieval works

--- src/main/scala/search/VerySimpleRetrieval.scala
+++ src/main/scala/search/VerySimpleRetrieval.scala
@@ -6,8 +6,8 @@ import scala.collection.immutable.Vector
 import scala.io.Source
 
 //Searches a directory of files
-//and prints a file that contains all
-//of the search terms
+//and prints the first part of any 
+//files that contains all the terms
 object VerySimpleRetrieval {
 	
 	def main(args: Array[String]) = {
@@ -15,13 +15,19 @@ object VerySimpleRetrieval {
 			println("ERROR: must supply a directory and at least one term")
 		}
 		val dir = new File(args(0))
-		println("DIR NAME IS " + dir)
-		println("DIR ENTRIES = " + dir.list().length)
-		println("DIR ENTRY ONE = " + dir.list()(0))
 		val index = indexDocsInDir(dir)
 		var searchTerms = Vector.empty[Token]
 		for (i <- 1 to args.length - 1) {
 			searchTerms = searchTerms :+ (new Token(args(i)))
+		}
+		val docsWithAllTerms = index.docsContainingAll(searchTerms)
+		if (docsWithAllTerms.size < 1) {
+			println("NO DOCUMENTS WITH ALL SEARCH TERMS")
+		} else {
+			for (doc <- docsWithAllTerms) {
+				println(doc.text.take(200))
+				println()
+			}
 		}
 	}
 	
