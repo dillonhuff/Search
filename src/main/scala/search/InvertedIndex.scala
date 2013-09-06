4653b2b7703cd9be9199c692e7dbcc3ecc658fab
Dillon Huff <dillonhuff@gmail.com>
2013-09-06 13:01:56 -0700

    moved Token inside Tokenizer source file

--- src/main/scala/search/InvertedIndex.scala
+++ src/main/scala/search/InvertedIndex.scala
@@ -46,21 +46,3 @@ class Document(txt: String) {
 		return text.hashCode
 	}
 }
-
-class Token(s: String) {
-	val str: String = s
-	
-	override
-	def equals(obj: Any): Boolean = {
-		if (!obj.isInstanceOf[Token]) {
-			return false
-		}
-		val other = obj.asInstanceOf[Token]
-		return this.str.equals(other.str)
-	}
-	
-	override
-	def hashCode(): Int = {
-		return str.hashCode
-	}
-}
--- src/main/scala/search/Tokenizer.scala
+++ src/main/scala/search/Tokenizer.scala
@@ -3,3 +3,21 @@ package search
 trait Tokenizer {
 	def tokenize(doc: Document): Vector[Token]	
 }
+
+class Token(s: String) {
+	val str: String = s
+	
+	override
+	def equals(obj: Any): Boolean = {
+		if (!obj.isInstanceOf[Token]) {
+			return false
+		}
+		val other = obj.asInstanceOf[Token]
+		return this.str.equals(other.str)
+	}
+	
+	override
+	def hashCode(): Int = {
+		return str.hashCode
+	}
+}
