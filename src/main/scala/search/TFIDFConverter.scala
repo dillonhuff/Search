3ce43fcd50bfd442251de67175ab9ee62e817aa1
dillonhuff <dillonhuff@gmail.com>
2013-09-08 18:49:42 -0500

    about to refactor FeatureVector to take Map[Token, Double]

---
+++ src/main/scala/search/TFIDFConverter.scala
@@ -0,0 +1,8 @@
+package search
+
+class TFIDFConverter(tokenizer: Tokenizer, docFreqs: Map[Token, Int], numDocs: Int) extends DocToFeatureVector {
+      
+      def convert(doc: Document): FeatureVector = {
+      	  return new FeatureVector(Map.empty[String, Double])
+      }
+}
---
+++ src/test/scala/search/TFIDFConverterTests.scala
@@ -0,0 +1,27 @@
+package search
+
+import org.junit.Assert._
+import org.junit.Test
+
+class TFIDFConverterTests {
+      
+      @Test
+      def convertEmptyDoc() = {
+      	  val doc = new Document("")
+	  val tfIdfConv = new TFIDFConverter(new WhitespaceTokenizer(), Map.empty[Token, Int], 1)
+	  val correct = new FeatureVector(Map.empty[String, Double])
+	  assertEquals(correct, tfIdfConv.convert(doc))
+      }
+
+      @Test
+      def convertNonEmptyDoc() = {
+      	  val doc = new Document("the man is the hat")
+	  val totalDocs = 100
+	  val docFreqs = Map(new Token("the") -> 100, new Token("man") -> 4, new Token("is") -> 99, new Token("hat") -> 1)
+	  val tdIdfConv = new TFIDFConverter(new WhitespaceTokenizer(), docFreqs, totalDocs)
+	  val correct = new FeatureVector(Map(new Token("the") -> Math.log(100/100)/Math.log(2),
+	      new Token("man") -> Math.log(100/4)/Math.log(2), new Token("is") -> Math.log(100/99)/Math.log(2),
+	      new Token("hat") -> Math.log(100/1)/Math.log(2)))
+	  assertEquals(correct, tfIdfConv.convert(doc))
+      }
+}
