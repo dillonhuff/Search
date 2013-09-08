37d37bc943c6c8ab28e83b11b8f99f7e58288092
dillonhuff <dillonhuff@gmail.com>
2013-09-08 17:58:31 -0500

    DocumentCollection.docFrequency passes tests

--- src/main/scala/search/DocumentCollection.scala
+++ src/main/scala/search/DocumentCollection.scala
@@ -2,4 +2,24 @@ package search
 
 class DocumentCollection(docs: Vector[Document]) {
 	val documents = docs
+	val tokenizer = new WhitespaceTokenizer()
+
+	val documentFrequencies = computeDocFrequencies(docs)
+	
+	def docFrequency(token: Token): Int = {
+	    documentFrequencies.getOrElse(token, 0)
+	}
+
+	def computeDocFrequencies(docs: Vector[Document]): Map[Token, Int] = {
+	    val docUniqueTokens = docs.map(tokenizer.tokenize(_).distinct.toSet)
+	    return docUniqueTokens.foldLeft(Map.empty[Token, Int])(addCounts)
+	}
+
+	def addCounts(tokenDocFrequencies: Map[Token, Int], tokens: Set[Token]): Map[Token, Int] = {
+	    var newTokenDocFreqs = tokenDocFrequencies
+	    for (token <- tokens) {
+	    	newTokenDocFreqs += token -> (newTokenDocFreqs.getOrElse(token, 0) + 1)
+	    }
+	    return newTokenDocFreqs
+	}
 }
