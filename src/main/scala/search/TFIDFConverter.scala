77d2f0469287c850e8580f764cd9a795caf64be8
dillonhuff <dillonhuff@gmail.com>
2013-09-08 19:36:52 -0500

    tf-idf converter passes tests

--- src/main/scala/search/TFIDFConverter.scala
+++ src/main/scala/search/TFIDFConverter.scala
@@ -3,6 +3,18 @@ package search
 class TFIDFConverter(tokenizer: Tokenizer, docFreqs: Map[Token, Int], numDocs: Int) extends DocToFeatureVector {
       
       def convert(doc: Document): FeatureVector = {
-      	  return new FeatureVector(Map.empty[Token, Double])
+      	  val tokens = tokenizer.tokenize(doc)
+	  val tokenCounts = tokens.foldLeft(Map.empty[Token, Int])((counts, token) => 
+	      counts + (token -> (counts.getOrElse(token, 0) + 1)))
+	  val tokensToTFIDFWeights = tokenCounts.map({case (token, termFreq) => 
+	      (token, tfIdfWeight(termFreq.toDouble, docFreqs.getOrElse(token, 0).toDouble))})
+      	  return new FeatureVector(tokensToTFIDFWeights)
+      }
+
+      def tfIdfWeight(termFreq: Double, docFreq: Double): Double = {
+      	  if (termFreq == 0 || docFreq == 0) {
+	     return 0.0
+	  }
+	  return termFreq*Math.log(numDocs/docFreq)/Math.log(2)
       }
 }
--- src/test/scala/search/TFIDFConverterTests.scala
+++ src/test/scala/search/TFIDFConverterTests.scala
@@ -19,9 +19,9 @@ class TFIDFConverterTests {
 	  val totalDocs = 100
 	  val docFreqs = Map(new Token("the") -> 100, new Token("man") -> 4, new Token("is") -> 99, new Token("hat") -> 1)
 	  val tfIdfConv = new TFIDFConverter(new WhitespaceTokenizer(), docFreqs, totalDocs)
-	  val correct = new FeatureVector(Map(new Token("the") -> Math.log(100/100)/Math.log(2),
-	      new Token("man") -> Math.log(100/4)/Math.log(2), new Token("is") -> Math.log(100/99)/Math.log(2),
-	      new Token("hat") -> Math.log(100/1)/Math.log(2)))
+	  val correct = new FeatureVector(Map(new Token("the") -> Math.log(100.0/100.0)/Math.log(2.0),
+	      new Token("man") -> Math.log(100.0/4.0)/Math.log(2.0), new Token("is") -> Math.log(100.0/99.0)/Math.log(2.0),
+	      new Token("hat") -> Math.log(100.0/1.0)/Math.log(2.0)))
 	  assertEquals(correct, tfIdfConv.convert(doc))
       }
 }
